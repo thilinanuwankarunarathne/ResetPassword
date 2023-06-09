30d071ec65f7d23a1368c9bd21b2b80219ab2394
thilina22 <test@123gmail.com>
2023-06-09 20:03:20 +0530

    push

---
+++ src/main/java/codingpentagon/controller/UserController.java
@@ -0,0 +1,35 @@
+package codingpentagon.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import codingpentagon.service.UserService;
+
+@RestController
+
+public class UserController {
+
+   
+    private UserService userService;
+
+    @PostMapping("/forget-password")
+	public String forgotPassword(@RequestParam("email") String email) {
+		System.out.println("fsfsdf");
+		String response = userService.forgotPassword(email);
+
+		if (!response.startsWith("Invalid")) {
+			response = "http://localhost:8080/reset-password?token=" + response;
+		}
+		return response;
+	}
+
+	@PutMapping("/reset-password")
+	public String resetPassword(@RequestParam String token,
+			@RequestParam String password) {
+
+		return userService.resetPassword(token, password);
+	}
+    
+}
